696b0e14949b09d9645275f100f41b2614444941
Suraj K P <surajkp3685@gmail.com>
2025-06-16 20:36:43 +0530

    Updated My Movie Suggestion App

---
+++ src/contexts/MovieContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const MovieContext = createContext()
+
+export const useMovieContext = () => useContext(MovieContext)
+
+export const MovieProvider = ({ children }) => {
+    const [favorites, setFavourites] = useState([])
+
+    useEffect(() => {
+        const storedFavs = localStorage.getItem("Favorites")
+        if (storedFavs) setFavourites(JSON.parse(storedFavs))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }, [favorites])
+
+    const addToFavorites = (movie) => {
+        setFavourites(prev => [...prev, movie])
+    }
+
+    const removeFromFavorites = (movieId) => {
+        setFavourites(prev => prev.filter(movie => movie.id !== movieId))
+    }
+
+    const isFavorite = (movieId) => {
+        return favorites.some(movie => movie.id === movieId)
+    }
+
+    const value = {
+        favorites,
+        addToFavorites,
+        removeFromFavorites,
+        isFavorite
+    }
+
+    return <MovieContext.Provider value={value}>
+        {children}
+    </MovieContext.Provider>
+}
+
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "13955e90745b30d8db0bfa415bb80d9c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query)}`
+    );
+    const data = await response.json()
+    return data.results
+};
